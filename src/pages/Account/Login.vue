baaacf120a66872b441e8b5c4fef231f4cf0daaf
Asiasoft-Szebing <szebing@asiasoft.my>
2025-03-17 18:05:09 +0800

    20250317

--- src/pages/Account/Login.vue
+++ src/pages/Account/Login.vue
@@ -6,4 +6,19 @@
 import {ref} from 'vue'
 
 import PlainLayout from '/src/layout/PlainLayout.vue'
+
+// State for form fields and error messages
+const email = ref("");
+const password = ref("");
+const showPassword = ref(false);
+const rememberMe = ref(false);
+const emailError = ref("");
+const passwordError = ref("");
+const loginError = ref("");
+
+// Show Password
+const togglePasswordVisibility = () => {
+  showPassword.value = !showPassword.value;
+};
+
 </script>
